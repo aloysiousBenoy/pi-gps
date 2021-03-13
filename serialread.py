4ec9fc27734bbcfbab0d80e68a853345efd82a30
Aloysious Benoy <aloysiousbenoy@gmail.com>
2021-03-13 05:31:05 +0000

    Initial commit

---
+++ serialread.py
@@ -0,0 +1,13 @@
+import serial
+from time import sleep
+
+ser = serial.Serial("/dev/ttyS0", 9600)  # Open port with baud rate
+a = 0
+while True:
+    received_data = ser.read()  # read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()  # check for remaining byte
+    received_data += ser.read(data_left)
+    a += 1
+    print("{}".format(a), end='')
+    print(received_data)
